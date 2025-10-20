e202c480ba3dc31800c73f77c8b5008e2a32ece9
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2025-10-20 20:35:04 +0600

    fixed transaction response

--- src/modules/Transactions/Transactions.controller.ts
+++ src/modules/Transactions/Transactions.controller.ts
@@ -1,53 +1,50 @@
-import { StatusCodes } from "http-status-codes";
-import catchAsync from "../../shared/catchAsync";
-import sendResponse from "../../shared/sendResponse";
-import { TransactionService } from "./Transactions.service";
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../shared/catchAsync';
+import sendResponse from '../../shared/sendResponse';
+import { TransactionService } from './Transactions.service';
 
 // Transactions.controller.ts
 const makePayment = catchAsync(async (req, res) => {
-
-    req.body.user = req.User.userId;
-    const result = await TransactionService.makePayment(req.body);
-    sendResponse(res, {
-        code: StatusCodes.CREATED,
-        message: 'Transaction created successfully',
-        data: result,
-    });
-})
-
+  req.body.user = req.User.userId;
+  const result = await TransactionService.makePayment(req.body);
+  sendResponse(res, {
+    code: StatusCodes.CREATED,
+    message: 'Transaction created successfully',
+    data: result ? result : {},
+  });
+});
 
 const getAllTransaction = catchAsync(async (req, res) => {
-    const result = await TransactionService.getAllTransaction(req.query);
-    sendResponse(res, {
-        code: StatusCodes.OK,
-        message: 'Transactions fetched successfully',
-        data: result,
-    });
-})
+  const result = await TransactionService.getAllTransaction(req.query);
+  sendResponse(res, {
+    code: StatusCodes.OK,
+    message: 'Transactions fetched successfully',
+    data: result && result.data.length > 0 ? result : [],
+  });
+});
 
 const getMyTransaction = catchAsync(async (req, res) => {
-    const result = await TransactionService.getMyTransaction(req.User.userId);
-    sendResponse(res, {
-        code: StatusCodes.OK,
-        message: 'Transactions fetched successfully',
-        data: result,
-    });
-})
+  const result = await TransactionService.getMyTransaction(req.User.userId);
+  sendResponse(res, {
+    code: StatusCodes.OK,
+    message: 'Transactions fetched successfully',
+    data: result && result.length > 0 ? result : [],
+  });
+});
 
 const getSingleTransaction = catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const result = await TransactionService.singleTransaction(id);
-    sendResponse(res, {
-        code: StatusCodes.OK,
-        message: 'Transaction fetched successfully',
-        data: result,
-    });
-})
-
+  const { id } = req.params;
+  const result = await TransactionService.singleTransaction(id);
+  sendResponse(res, {
+    code: StatusCodes.OK,
+    message: 'Transaction fetched successfully',
+    data: result ? result : {},
+  });
+});
 
 export const TransactionController = {
-    makePayment,
-    getAllTransaction,
-    getMyTransaction,
-    getSingleTransaction
-}
+  makePayment,
+  getAllTransaction,
+  getMyTransaction,
+  getSingleTransaction,
+};
